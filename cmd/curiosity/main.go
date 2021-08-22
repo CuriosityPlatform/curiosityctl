1d692dc3c89bc55c55ee150ae7f5186a92d0cf50
UsingCoding <extendedmoment@outlook.com>
2021-08-22 19:28:38 +0300

    Добавил описание к командам

--- cmd/curiosity/main.go
+++ cmd/curiosity/main.go
@@ -30,18 +30,37 @@ func runApp(ctx context.Context, args []string) error {
 	app := &cli.App{
 		Name:    appID,
 		Version: "1.0",
+		Usage:   "Tool to manage, init curiosity environment",
 		Commands: []*cli.Command{
 			{
-				Name:   "up",
-				Action: executeUp,
+				Name:        "up",
+				Action:      executeUp,
+				Usage:       "UP environment",
+				Description: "UP curiosity environment via docker-compose.\nUses configuration based on docker context.\nRequired installed 'Platform'",
 			},
 			{
-				Name:   "down",
-				Action: executeDown,
+				Name:        "down",
+				Action:      executeDown,
+				Usage:       "DOWN environment",
+				Description: "DOWN curiosity environment",
 			},
 			{
-				Name:   "restart",
-				Action: executeRestart,
+				Name:        "restart",
+				Action:      executeRestart,
+				Usage:       "RESTART environment",
+				Description: "RESTART curiosity environment",
+			},
+			{
+				Name:            "install",
+				Usage:           "Install curiosity environment modules",
+				Description:     "Install curiosity environment modules, like Platform and one or all services",
+				HideHelpCommand: true,
+				Subcommands: []*cli.Command{
+					{
+						Name:     "platform",
+						Category: "install",
+					},
+				},
 			},
 		},
 	}
